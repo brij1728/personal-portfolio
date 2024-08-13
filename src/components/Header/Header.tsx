772b508b8b02053ff2634b3d0cc4360423fd3b1a
Brijesh Kumar <bk10895@gmail.com>
2024-08-13 16:28:38 +0530

    feat: add Montserrat font and update UI styling
    - Add Montserrat font to the project
    - Refactor Header component to use the new font and update styling
    - Update tailwind.config.ts to include new color palette and font family
    - Rearrange package.json dependencies for better organization

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link';
 export const Header = () => (
   <header className="p-4 bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
     <nav className="flex justify-between items-center max-w-7xl mx-auto">
-      {/* Logo Section */}
       <div className="flex items-center space-x-2">
         <Image
           src="/logo.png" 
@@ -12,10 +11,15 @@ export const Header = () => (
           width={40}
           height={40}
         />
-        <span className="text-xl font-bold">Mumair</span>
+        <span className="text-4xl font-semibold text-secondary-200 font-montserrat">
+            M
+         <span className="text-secondary-300 text-xl font-normal tracking-widest">
+            umair
+          </span>
+        </span>
+
       </div>
 
-      {/* Navigation Links */}
       <ul className="flex space-x-8">
         <li><Link href="#home" className="hover:text-primary">Home</Link></li>
         <li><Link href="#about" className="hover:text-primary">About Me</Link></li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,36 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: '#F97316', 
+        primary: '#FD6F00', 
+        secondary: {
+          100: '#000000',
+          200: '#1E1E1E',
+          300: '#545454',
+        },
+        orange: {
+          50: '#fff6e6',
+          100: '#ffcb90',
+          200: '#ffb78a',
+          300: '#ff9654',
+          400: '#ff8233',
+          500: '#ff6300',
+          600: '#e85a00',
+          700: '#b54600',
+          800: '#8c3600',
+          900: '#6b2a00',
+        },
+        black: {
+          50: '#e6e6e6',
+          100: '#b0b0b0',
+          200: '#8a8a8a',
+          300: '#545454',
+          400: '#333333',
+        
+        },
+      
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], 
       },
     },
   },

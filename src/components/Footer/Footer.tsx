6b1c3a57ad7cf1da44ec9108bce0a56dc9bc9d2d
Brijesh Kumar <bk10895@gmail.com>
2024-08-13 17:14:10 +0530

    style: update footer copyright text and styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => (
   <footer className="p-4 bg-secondary-300 dark:bg-secondary-400 text-primary-300 flex justify-center items-center ">
-    <p>&copy; 2024 <span className="text-primary-100 font-semibold">Mumair</span>. All rights reserved.</p>
+    <p className="text-sm font-light">&copy; 2023 <span className="text-primary-100 font-semibold">Mumair</span> All Rights Reserved , Inc.</p>
   </footer>
 );
 
